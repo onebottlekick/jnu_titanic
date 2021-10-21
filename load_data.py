92ff340e83eeb0a2f4994e117f8b209041d12376
onebottlekick <gksqudcks97@gmail.com>
2021-10-21 17:57:17 +0900

    data preprocess with pandas

--- load_data.py
+++ load_data.py
@@ -1,85 +1,45 @@
-import csv
-import random
-import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
-def preprocess_sex(txt):
-    if txt == 'male':
-        txt = 0
-    elif txt == 'female':
-        txt = 1
-    return float(txt)
+def normalize(df):
+    result = df.copy()
+    for feature_name in df.columns:
+        max_value = df[feature_name].max()
+        min_value = df[feature_name].min()
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    return result
 
 
-def preprocess_age(txt):
-    if txt == '':
-        txt = 29.6
-    return float(txt)
+def data():    
+    train_data = pd.read_csv('./dataset/train.csv')
+    submit_data = pd.read_csv('./dataset/test.csv')
+    label = train_data.Survived.to_numpy()
+    id_num = submit_data.PassengerId.to_numpy()
 
+    selected_features = ['Sex', 'Age', 'Pclass', 'Fare']
 
-# one-hot encode
-def to_one_hot(X):
-    n_X = int(np.max(X) + 1)
-    return np.eye(n_X)[X.astype(np.int)]
+    train_data = train_data[selected_features]
+    submit_data = submit_data[selected_features]
 
+    # feature process
+    label_en = LabelEncoder()
+    train_data.Sex = label_en.fit_transform(train_data.Sex)
+    submit_data.Sex = label_en.transform(submit_data.Sex)
 
-class DataReader:
-    def __init__(self, kind, test_ratio=0.2):
-        self.test_ratio = test_ratio
-        self.kind = kind
+    # null process
+    train_data = normalize(train_data.interpolate(method='linear', limit_direction='forward'))
+    submit_data = normalize(submit_data.interpolate(method='linear', limit_direction='forward'))
 
-        if self.kind == 'train':
-            self.X_train, self.X_test, self.y_train, self.y_test = self.read_data()        
-            print('X_train shape:', self.X_train.shape)
-            print('X_test shape:', self.X_test.shape)
-            print('y_train shape:', self.y_train.shape)
-            print('y_test shape:', self.y_test.shape)
+    X = train_data.to_numpy()
+    submit_X = submit_data.to_numpy()
 
-        if self.kind == 'test':
-            self.id_num, self.data = self.read_data()
+    # label process
+    onehot_en = OneHotEncoder()
+    y = onehot_en.fit_transform(label.reshape(-1, 1)).toarray()
 
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    def read_data(self):        
-        with open(f'dataset/{self.kind}.csv', 'r') as f:
-            data = []
-            id_num = []
-            f.readline()
-            file = csv.reader(f)
-
-            # process assignment data
-            if self.kind == 'test':
-                for line in file:
-                    PassengerId = int(line[0])
-                    # Pclass = process(line[2])
-                    Sex = preprocess_sex(line[3])
-                    Age = preprocess_age(line[4])
-                    # Sibsp = process(line[6])
-                    # Parch = process(line[7])
-                    # Fare = process(line[9])
-                    data.append([Sex, Age])
-                    id_num.append(PassengerId)
-                    
-                return id_num, data
-
-            # process train data
-            for line in file:
-                # Pclass = process(line[2])
-                Sex = preprocess_sex(line[4])
-                Age = preprocess_age(line[5])
-                # Sibsp = process(line[6])
-                # Parch = process(line[7])
-                # Fare = process(line[9])
-                Survived = float(line[1])
-                data.append((Sex, Age, Survived))
-                
-            random.shuffle(data)
-            data = np.asarray(data)
-            norm_data = data/np.max(data, axis=0)
-            num_row = norm_data.shape[0]
-            X_train = norm_data[:int(num_row*(1 - self.test_ratio)), :-1]
-            y_train = to_one_hot(data[:int(num_row*(1 - self.test_ratio)), -1])
-            X_test = norm_data[int(num_row*(1 - self.test_ratio)):, :-1]
-            y_test = to_one_hot(data[int(num_row*(1 - self.test_ratio)):, -1])
-
-        return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test, submit_X, id_num
 
